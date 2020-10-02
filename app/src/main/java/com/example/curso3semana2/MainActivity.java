572b0e34258fa1267d6adc4aedd66f7a9ae8d54d
Ines <ines@nucleartesuji.com>
2020-10-01 21:43:35 -0300

    subo para evaluaciòn coursera

---
+++ app/src/main/java/com/example/curso3semana2/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.curso3semana2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button siguiente=findViewById(R.id.boton_siguiente);
+        siguiente.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent=new Intent(this,ConfirmacionActivity.class);
+        TextView nombre=findViewById(R.id.nombre);
+        intent.putExtra("nombre",nombre.getText().toString());
+
+        DatePicker nacimiento=findViewById(R.id.nacimiento);
+        intent.putExtra("nacimientodia",nacimiento.getDayOfMonth());
+        intent.putExtra("nacimientomes",nacimiento.getMonth());
+        intent.putExtra("nacimientoano",nacimiento.getYear());
+
+        TextView telefono=findViewById(R.id.telefono);
+        intent.putExtra("telefono",telefono.getText().toString());
+
+        TextView email=findViewById(R.id.email);
+        intent.putExtra("email",email.getText().toString());
+
+        TextView descripcioncontacto=findViewById(R.id.descripcioncontacto);
+        intent.putExtra("descripcioncontacto",descripcioncontacto.getText().toString());
+        startActivity(intent);
+    }
+}
